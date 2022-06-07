2ab4be6a44bcab5f9c9184a437ca4d1e824bf590
KekOnTheWorld <kekontheworld@gmail.com>
2022-06-07 15:14:04 +0200

    Added sample

---
+++ earthplay.sh
@@ -0,0 +1,6 @@
+# Created on Tue Jun 07 2022
+#
+# Copyright (c) 2022 EarthPlay
+
+# Sample command for creating the sphere
+python3 dynsphere/ -b https://dynmap.earthplay.de/ --mintz -11 --maxtz 12 --mintx -24 --maxtx 23 --level 100 --resx 2048 --resy 2048
